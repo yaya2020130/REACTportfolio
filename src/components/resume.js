97a4817117378a4e84886187a738a307edd349d5
yaya <anbesie.yg@gmail.com>
2020-05-16 01:39:05 -0700

    u

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Grid, Cell } from 'react-mdl'
 import Education from './Education';
+import Experience from './Experience';
+
+import Skills from './skills';
 class Resume extends React.Component {
   render() {
     return (
@@ -29,12 +32,34 @@ class Resume extends React.Component {
 
           </Cell> 
         <Cell className='resume-right-col ' col={8} >
+<h2>Education</h2>
               <Education
+
               startYear={2007}
            endYear={2012}
            schoolName="Addis Ababa University"
            schoolDescription="The Addis Ababa Institute of Technology (AAiT), which was established about 60 years ago, is the leading Institute of Technology in Ethiopia.At present, it is functioning with special autonomy from Addis Ababa University. It has a Supervisory Board, which is the highest governing body. The Institute is led by a Scientific Director with the rank of University Vice President."
            />
+
+         <Education
+              startYear={2020}
+           endYear={2020}
+           schoolName="Addis Ababa UniversityUniversity of washington Coding Bootcamp"
+           schoolDescription="University of Washington Coding Boot Camp equips students with the core skills needed for a career in web development and technology through engaging classes."
+           />
+           <hr style={{borderTop:'3px solid#22947'}}/>
+
+<h2>Experience</h2>
+           <Experience
+           startYear={2018}
+           endYear={2020}
+           jobName={"Home appliance technician"}
+           job description ={"Maintaining home apliances"}
+
+   
+           />
+<h1>Skills</h1>
+<Skills/>
         </Cell>
         </Grid>
       </div >
---
+++ src/components/skills.js
@@ -0,0 +1,20 @@
+import React, {Component}from 'react';
+import {Grid,Cell} from 'react-mdl';
+class Skills extends Component{
+ render(){
+   return(
+   <Grid>
+     <Cell col={12}>
+<div style={{display:'flex'}}>{this.props.Skills} </div>
+
+
+
+
+
+     </Cell>
+   </Grid>
+   )
+ }
+
+}
+export default Skills;
